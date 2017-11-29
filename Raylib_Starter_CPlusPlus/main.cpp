14b4a3dc858e43a0f28fd6fdcf4245823ad97397
braedenf <braedenfoster@Braedens-MacBook-Pro.local>
2017-11-29 17:13:59 +1300

    Initial Commit

---
+++ Raylib_Starter_CPlusPlus/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Raylib_Starter_CPlusPlus
+//
+//  Created by Braeden Foster on 29/11/17.
+//  Copyright © 2017 Braeden Foster. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
