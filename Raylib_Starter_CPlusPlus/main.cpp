bbd113843b7cbcbbbd02e88ce4a08762af9702d8
Braeden Foster <braedenleefoster@gmail.com>
2017-11-30 15:08:55 +1300

    Fixed up paths, so the libraries can be found from the root by going back two folders. Cleaned up main.pp.

--- Raylib_Starter_CPlusPlus/main.cpp
+++ Raylib_Starter_CPlusPlus/main.cpp
@@ -1,29 +1,27 @@
-//
-//  main.cpp
-//  Raylib_Starter_CPlusPlus
-//
-//  Created by Braeden Foster on 29/11/17.
-//  Copyright © 2017 Braeden Foster. All rights reserved.
-//
 
 #include "raylib.h"
 
 int main() {
-    //Initialise
+    
+    //window settings
     int screenWidth = 800;
     int screenHeight = 450;
     
-    InitWindow(screenWidth, screenHeight, "Raylib - Empty Project");
+    InitWindow(screenWidth, screenHeight, "Raylib - empty-project");
     
-    //Main game loop
+    SetTargetFPS(60); //Set target FPS
+    
+    //[-----------------start game loop--------------------]
     while (!WindowShouldClose()) {
-        //Update game loop
-        
-        BeginDrawing(); //start
-            ClearBackground(RAYWHITE); //Every frame clear background with white
-            DrawText("Start your game here...", 190, 200, 20, LIGHTGRAY);
-        EndDrawing();//end
+        //[------------Draw--------------]
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Start your game...", screenWidth/2.5, screenHeight/2, 20, LIGHTGRAY);
+        EndDrawing();
+        //[----------End Draw------------]
     }
-    //De-Initialise
-    CloseWindow();
+    //[---------------De-initialise Window-----------------]
+    CloseWindow(); //both window and OpenGL context
+    
 }
+
